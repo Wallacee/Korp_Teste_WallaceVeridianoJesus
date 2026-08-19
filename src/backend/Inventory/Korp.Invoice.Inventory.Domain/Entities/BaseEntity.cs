c91264faac063ac46b1abd07af54c47abe3bec89
Wallace Veridiano de Jesus <wallacev2@gmail.com>
2026-08-18 23:10:21 -0300

    feat: controle temporal de estoque

--- src/backend/Inventory/Korp.Invoice.Inventory.Domain/Entities/BaseEntity.cs
+++ src/backend/Inventory/Korp.Invoice.Inventory.Domain/Entities/BaseEntity.cs
@@ -3,4 +3,13 @@ namespace Korp.Invoice.Inventory.Domain.Entities;
 public abstract class BaseEntity
 {
     public Guid Id { get; protected set; } = Guid.NewGuid();
+
+    public DateTime CreatedAtUtc { get; protected set; } = DateTime.UtcNow;
+
+    public DateTime? UpdatedAtUtc { get; protected set; }
+
+    protected void MarkAsUpdated()
+    {
+        UpdatedAtUtc = DateTime.UtcNow;
+    }
 }
