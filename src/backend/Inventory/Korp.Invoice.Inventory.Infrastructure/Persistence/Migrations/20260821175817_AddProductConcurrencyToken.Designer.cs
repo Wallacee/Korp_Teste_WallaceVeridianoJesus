378cd144919155dc4dac4eb38fdae3bc1492ac58
Wallace Veridiano de Jesus <wallacev2@gmail.com>
2026-08-21 15:06:14 -0300

    core: ajustes de infra e migration

--- src/backend/Inventory/Korp.Invoice.Inventory.Infrastructure/Persistence/Migrations/20260821175817_AddProductConcurrencyToken.Designer.cs
+++ src/backend/Inventory/Korp.Invoice.Inventory.Infrastructure/Persistence/Migrations/20260821175817_AddProductConcurrencyToken.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Korp.Invoice.Inventory.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Korp.Invoice.Inventory.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(InventoryDbContext))]
-    partial class InventoryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260821175817_AddProductConcurrencyToken")]
+    partial class AddProductConcurrencyToken
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
